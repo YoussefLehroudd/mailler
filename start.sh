0aff299138414d3daf5895bd94be26bbb39c0dd9
Youssef <Youssef@hrd.com>
2025-10-01 21:28:10 +0100

    add start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start PHP built-in web server
+# Default port is 8080, but Railpack may set PORT environment variable
+PORT=${PORT:-8080}
+
+echo "Starting PHP server on port $PORT..."
+php -S 0.0.0.0:$PORT -t .
